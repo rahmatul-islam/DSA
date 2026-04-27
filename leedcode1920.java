223f9276f3e9103989ab82fd1d370b15ed7e4cb8
RAHMATUL ISLAM <rahmatulislam.se@gmail.com>
2026-04-27 11:36:16 +0600

    static dile eivabe korte hobe

--- leedcode1920.java
+++ leedcode1920.java
@@ -3,14 +3,13 @@ import java.util.Arrays;
 public class leedcode1920{
 public static void main(String[] args) {
     int [] nums = {0,2,1,5,3,4};
-    Solution solution = new Solution();
-    int[] result = solution.buildArray(nums);
+    int[] result = Solution.buildArray(nums);
     System.out.println(Arrays.toString(result));
 }
 
 }
 class Solution {
-    public int[] buildArray(int[] nums) {
+    public static int[] buildArray(int[] nums) {
         int n=nums.length;
         int [] ans=new int[n];
         for (int i = 0; i < nums.length; i++) {
